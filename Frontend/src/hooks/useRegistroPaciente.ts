9b6542d6f159bb15be8950c9af68a7c34381a680
spartanpotato <107315314+spartanpotato@users.noreply.github.com>
2026-06-24 12:30:01 -0400

    prueba asesorias grupo10

--- Frontend/src/hooks/useRegistroPaciente.ts
+++ Frontend/src/hooks/useRegistroPaciente.ts
@@ -10,7 +10,9 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [new Sentry.BrowserTracing()],
+  integrations: [
+    Sentry.browserTracingIntegration(),
+  ],
   tracesSampleRate: 1.0,
 });
 
